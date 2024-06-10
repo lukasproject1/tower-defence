3086c2ef325e6e30157cec8aec4f53d82ba3acc8
lukasproject1 <2187633@roc-dev.com>
2024-06-10 13:45:56 +0200

    latest update

--- Assets/Scripts/HpCave.cs
+++ Assets/Scripts/HpCave.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using TMPro;
 
 public class HpCave : MonoBehaviour
 {
-    public int health= 100;
+    static void main(string[] args)
+    {
+
+        HpClass a = HpClass.GetInstance();
+        HpClass b = HpClass.GetInstance();
+        HpClass c = HpClass.GetInstance();
+
+    }
+
+    public int health = 100;
+    public TextMeshProUGUI healthText;
     // Start is called before the first frame update
     void Start()
     {
-        
-       
+        healthText.text = "Health: " + health;
+
     }
 
     // Update is called once per frame
@@ -28,13 +39,16 @@ public class HpCave : MonoBehaviour
         {
             if (hit.collider != null)
             {
+
                 // Check if the hit object is on the "light enemy" layer
                 if (hit.collider.gameObject.layer == layerLightEnemy)
                 {
                     // Reduce health by 1
                     health -= 1;
+                    healthText.text = "Health: " + health; // Update the health text
                     Debug.Log("Hit light enemy, health reduced to: " + health);
                 }
+
 
                 // Destroy the gameobject that was hit by the raycast
                 Destroy(hit.collider.gameObject);
@@ -43,4 +57,52 @@ public class HpCave : MonoBehaviour
             }
         }
     }
+
+
+    internal class HpClass
+    {
+        private int Gamehealth = 100;
+        private static HpClass _instance;
+        private HpClass()
+        {
+
+        }
+        public static HpClass GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new HpClass();
+
+            }
+            return _instance;
+        }
+        public void Hp(int Ghealth)
+        {
+            Gamehealth -= Ghealth;
+        }
+    }
+
+    internal class ScoreM
+    {
+
+        private static ScoreM _instance;
+        private int score = 0;
+        private ScoreM() { }
+        public static ScoreM GetInstance()
+        {
+
+            if (_instance == null)
+            {
+                _instance = new ScoreM();
+            }
+            return _instance;
+        }
+        public void AddScore(int someScore)
+        {
+            score += someScore;
+        }
+    }
 }
+    
+
+
